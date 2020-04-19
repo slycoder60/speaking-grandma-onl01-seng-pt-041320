73e4c86c06392f3358a6adb5a720b2328d5bbd22
Omar Farah <farah.omar8787@gmail.com>
2020-04-19 18:35:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase.upcase
-  "HUH?! SPEAK UP, SONNY!" 
-elsif phrase == phrase.upcase 
+  "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.upcase
   "NO, NOT SINCE 1938!"
 
 end
