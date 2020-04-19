eb3ba088357d4901fe5da0776a45d406c1616b50
Omar Farah <farah.omar8787@gmail.com>
2020-04-19 17:10:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,7 @@
 # Write a speak_to_grandma method.
+speak_to_grandma("phrase")
+if "phrase"
+  puts "HUH?! SPEAK UP SONNY!"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
