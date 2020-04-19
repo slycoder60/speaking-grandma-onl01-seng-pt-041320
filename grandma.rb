a610f10e992806e17bb0780dc4a558d59146fabd
Omar Farah <farah.omar8787@gmail.com>
2020-04-19 18:35:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,8 @@ if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase.upcase
   "HUH?! SPEAK UP, SONNY!" 
+elsif phrase == phrase.upcase 
+  "NO, NOT SINCE 1938!"
 
 end
 end
